8cb10e703893e63477b7539cc6cdabe4eeb9c2b1
kimmo1019 <liu-q16@mails.tsinghua.edu.cn>
2020-04-29 04:23:24 +0800

    update

--- prog/get_deepcage_input_from_gwas.py
+++ prog/get_deepcage_input_from_gwas.py
@@ -56,6 +56,85 @@ def get_data(use_indel):
     f_out.close()
     f_gwas.close()
         
+def get_all_data():
+    genotype_head_file = '%s/GTEx/GTEx_WGS_genotype_first500.txt' %DPATH
+    muscle_info = '%s/GTEx/GTEx_rnaseq_mucsle.txt'%DPATH
+    first500 = open(genotype_head_file).readlines()
+    line = first500[1].strip().split('\t')
+    donor_muscle_ids = [item.split('\t')[0] for item in open(muscle_info).readlines()]
+    donor_ids = ['-'.join(item.rstrip().split('-')[:2]) for item in donor_muscle_ids]
+    muscle_tissue_ids=[donor_muscle_ids[donor_ids.index(item)] for item in line[9:] if item in donor_ids]
+
+    tf_gexp_file = '%s/GTEx/GTEx_rseq_tpm_peca.csv'%DPATH
+    tf_gexp = pd.read_csv(tf_gexp_file ,sep='\t',header=0,index_col=[0])
+    tf_gexp_log = np.log(tf_gexp+1)
+    tf_gexp_log = pd.DataFrame.transpose(quantile_norm(pd.DataFrame.transpose(tf_gexp_log)))
+
+    fasta_file = '%s/height_gwas/neighbor_var/%s.fa'%(DPATH,rs)
+    genome = Fasta(fasta_file)
+    motifscore_file = '%s/height_gwas/neighbor_var/%s.motifscore.txt'%(DPATH,rs)
+    tf_motifscore = pd.read_csv(motifscore_file,sep='\t',header=0,index_col=[0])
+    var_info = '%s/height_gwas/neighbor_var/%s.hkl'%(DPATH,rs)
+    match_file = '%s/height_gwas/neighbor_var/%s.log'%(DPATH,rs)
+    match_line = open(match_file).readlines()
+    if len(match_line)>0:
+        chrom_gwas, pos_gwas = match_line[0].strip().split('\t')[0],match_line[0].strip().split('\t')[1]
+    epsilon = 1e-10
+    seq_data_ref,seq_data_alt =[],[]
+    gexp_data_ref,gexp_data_alt =[],[]
+    motif_data_ref,motif_data_alt=[],[]
+
+    vars_list = hkl.load(var_info)
+    for var in vars_list:
+        chrom, pos, ref, alt, related_donor_idx = var[0], var[1], var[2], var[3], var[4]
+        if len(ref)==1 and len(alt)==1:
+            seq_ref_id = 'wgs_ref_chr%s_%s_%s_%s'%(chrom,pos,ref,alt)
+            seq_alt_id = 'wgs_alt_chr%s_%s_%s_%s'%(chrom,pos,ref,alt)
+            seq_mat_ref = one_hot_seq(genome[seq_ref_id])
+            seq_mat_alt = one_hot_seq(genome[seq_alt_id])
+            motifscore_ref = tf_motifscore.loc[seq_ref_id]
+            motifscore_alt = tf_motifscore.loc[seq_alt_id]
+            for i in related_donor_idx:
+                gexp = tf_gexp_log.loc[muscle_tissue_ids[i]]
+                seq_data_ref.append(seq_mat_ref)
+                seq_data_alt.append(seq_mat_alt)
+                motif_data_ref.append(motifscore_ref)
+                motif_data_alt.append(motifscore_alt)
+                gexp_data_ref.append(gexp)
+                gexp_data_alt.append(gexp)
+
+    seq_data_ref=np.stack(seq_data_ref)
+    seq_data_alt=np.stack(seq_data_alt)
+    motif_data_ref=np.stack(motif_data_ref)
+    motif_data_alt=np.stack(motif_data_alt)
+    gexp_data_ref = np.stack(gexp_data_ref)
+    gexp_data_alt = np.stack(gexp_data_alt)
+    print seq_data_ref.shape,seq_data_alt.shape,motif_data_ref.shape,motif_data_alt.shape,gexp_data_ref.shape,gexp_data_alt.shape
+    np.savez('%s/height_gwas/neighbor_var/%s.ref.npz'%(DPATH,rs),seq_data_ref,motif_data_ref,gexp_data_ref)
+    np.savez('%s/height_gwas/neighbor_var/%s.alt.npz'%(DPATH,rs),seq_data_alt,motif_data_alt,gexp_data_alt)
+
+def get_seq():
+    fasta_file = '%s/height_gwas/neighbor_var/%s.fa'%(DPATH,rs)
+    genome = Fasta(fasta_file)
+    var_info = '%s/height_gwas/neighbor_var/%s.hkl'%(DPATH,rs)
+    vars_list = hkl.load(var_info)
+    seq_data_ref,seq_data_alt=[],[]
+    for var in vars_list:
+        chrom, pos, ref, alt, related_donor_idx = var[0], var[1], var[2], var[3], var[4]
+        if len(ref)==1 and len(alt)==1:
+            seq_ref_id = 'wgs_ref_chr%s_%s_%s_%s'%(chrom,pos,ref,alt)
+            seq_alt_id = 'wgs_alt_chr%s_%s_%s_%s'%(chrom,pos,ref,alt)
+            seq_mat_ref = one_hot_seq(genome[seq_ref_id])
+            seq_mat_alt = one_hot_seq(genome[seq_alt_id])
+            for i in related_donor_idx:
+                seq_data_ref.append(seq_mat_ref)
+                seq_data_alt.append(seq_mat_alt)
+
+    seq_data_ref=np.stack(seq_data_ref)
+    seq_data_alt=np.stack(seq_data_alt)
+    print seq_data_ref.shape,seq_data_alt.shape
+    np.save('%s/height_gwas/neighbor_var/%s.seq.npy'%(DPATH,rs),seq_data_ref)
+    np.save('%s/height_gwas/neighbor_var/%s.seq.npy'%(DPATH,rs),seq_data_alt)
 
 
 if __name__=="__main__":
@@ -64,9 +143,9 @@ if __name__=="__main__":
     height_GWAS='%s/height_gwas/Meta-analysis_Wood_et_al+UKBiobank_2018_top_3290_from_COJO_analysis.txt'%DPATH
     GWAS_variants = '%s/height_gwas/neighbor_var'%DPATH
     rs, chrom_gwas, pos_gwas, test_allele, other_allele = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5]
-    get_data(use_indel=False)
+    #get_data(use_indel=False)#generate fasta
+    #get_all_data()#seq_mat, gexp, and motifscore
+    get_seq()#only seq_mat
 
-    #deepcage input
-    # X_test_mat = np.zeros((nb_regions,4,SEQ_LEN,1))
-    # X_test_gexp = np.zeros((nb_regions,NUM_TF))
-    # X_test_motif = np.zeros((nb_regions,NUM_TF))
+    
+
